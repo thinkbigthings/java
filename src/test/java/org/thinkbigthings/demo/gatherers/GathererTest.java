e3bb78c0f4e7cd00da132ff7a4716e9d2c776a83
Jason Young <thinkbigthings@gmail.com>
2024-10-05 09:57:17 -0400

    added some tests to explore built in gatherers

--- src/test/java/org/thinkbigthings/demo/gatherers/GathererTest.java
+++ src/test/java/org/thinkbigthings/demo/gatherers/GathererTest.java
@@ -2,7 +2,7 @@ package org.thinkbigthings.demo.gatherers;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
@@ -10,6 +10,8 @@ import java.util.function.Supplier;
 import java.util.stream.*;
 
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Gatherers.fold;
+import static java.util.stream.Gatherers.scan;
 
 
 /**
@@ -17,6 +19,8 @@ import static java.util.stream.Collectors.toList;
  * Ideas for Gatherers:
  * https://openjdk.org/jeps/473
  * https://www.reddit.com/r/java/comments/14le6tw/gatherers/
+ *
+ * https://cr.openjdk.org/~vklang/Gatherers.html
  *
  * https://www.youtube.com/watch?v=8fMFa6OqlY8
  *
@@ -44,34 +48,116 @@ public class GathererTest {
                 .forEach(System.out::println);
     }
 
+    private double average(List<Double> window) {
+        return window.stream().flatMapToDouble(DoubleStream::of).average().orElseThrow();
+    }
+
     @Test
-    public void testGathererAsSmoother() {
+    public void testGathererSlidingWindow() {
 
         // sliding window can be used for things like smoothing functions
+        // there are better curve-fitting algorithms, but this illustrates the idea
 
-        var trueVales = IntStream.range(1, 11)
-                .mapToDouble(i -> i)
+        var xValues = IntStream.range(1, 62)
+                .mapToDouble(i -> i / 10d)
                 .boxed()
                 .toList();
 
-        var noisyValues = trueVales.stream()
-                .map(i -> i + (Math.random()-0.5))
+        var yValues = xValues.stream()
+                .map(Math::sin)
+                .toList();
+
+        var noisyValues = yValues.stream()
+                .map(i -> i + ((0.4 * Math.random()) - 0.2))
                 .toList();
 
         var smoothedValues = noisyValues.stream()
                 .gather(Gatherers.windowSliding(5))
-                .map(window -> window.stream().flatMapToDouble(DoubleStream::of).average().orElseThrow())
+                .map(this::average)
                 .toList();
 
 
+        System.out.println("X Values");
+        xValues.forEach(System.out::println);
+
+        System.out.println("Noisy Values");
         noisyValues.forEach(System.out::println);
 
         System.out.println("Smoothed values");
-
         smoothedValues.forEach(System.out::println);
+
 
     }
 
+    @Test
+    public void testFold() {
+
+        // looking at the source code for fold() and scan() in Gatherers.java is helpful
+
+
+        //  reduce is a kind of fold. Reduction takes a stream and turns it into a single value.
+        //  Folding also does this, but it loosens the requirements:
+        //  1) that the return type is of the same type as the stream elements
+        //  2) that the combiner is associative
+        //  3) the initializer on fold is a generator function not a static value.
+//        System.out.println("Fold example: Fibonacci sequence");
+//        Stream.of(1, 2, 3, 4, 5)
+//                .gather(Gatherers.fold(() -> 1, (a, b) -> a * b))
+//                .forEach(System.out::println);
+
+
+        var freqMap = Arrays.stream("gatherer".split(""))
+                .gather(fold(HashMap<String, Integer>::new, (map, str) -> {
+                    // since we're using fold, we can mutate the map in place
+                    map.put(str, map.getOrDefault(str, 0) + 1);
+                    return map;
+                }))
+                .findFirst() // since we only get one element out of fold, we can just grab the first one
+                .orElseGet(HashMap::new);
+
+        System.out.println(freqMap);
+
+        record Histogram(SortedMap<Integer, Integer> map, int binSize) {
+            public Histogram(int binSize) {
+                this(new TreeMap<Integer, Integer>(), binSize);
+            }
+            public Histogram addValue(Integer value) {
+                int bin =  (value / binSize) * binSize; // get the lowest bin that is a multiple of binSize
+                map.put(bin, map.getOrDefault(bin, 0) + 1);
+                return this;
+            }
+        }
+
+        // see if we can construct a set of histogram bins for a given set of data
+        var hist = Stream.of(1,2,3,4,5,4,5,6,6,6,7,7,8,9)
+                .gather(fold(() -> new Histogram(2), Histogram::addValue))
+                .toList();
+
+        System.out.println(hist);
+    }
+
+    @Test
+    public void testGatherScan() {
+
+//        List<String> numberStrings = Stream.of(1,2,3,4,5,6,7,8,9)
+//                .gather(scan(() -> "", (string, number) -> string + number) )
+//                .toList();
+//
+//        System.out.println(numberStrings);
+
+        // create a frequency map
+        // the BiFunction could be extracted for readability
+        var freqMap = Arrays.stream("gatherer".split(""))
+                .gather(scan(HashMap<String, Integer>::new, (map, str) -> {
+                    // Create a new map based on the previous map to avoid mutating the original one
+                    HashMap<String, Integer> newMap = new HashMap<>(map);
+                    newMap.put(str, newMap.getOrDefault(str, 0) + 1);
+                    return newMap;
+                }))
+                .toList();
+
+        System.out.println(freqMap);
+    }
 
 
     // We can transform a Collector to a Gatherer that emits the result downstream,
@@ -107,7 +193,7 @@ public class GathererTest {
                 .forEach(System.out::println);
 
         words.stream()
-                .collect(Collectors.toList())
+                .collect(toList())
                 .stream()
                 .forEach(System.out::println);
 
@@ -115,6 +201,11 @@ public class GathererTest {
         // how is this different from simply using a collector and then .stream() ?
         // advantage is not having to create a new stream?
         // maybe this is more useful for other collectors?
+
+        // non-terminal operations are more of a builder pattern that don't actually evaluate anything
+        // collect is a terminal operation that evaluates the stream.
+        // but we're using the collector's internals which will accumulate the whole stream anyway,
+        // so I'm still not sure what this idea buys us
 
 
     }
